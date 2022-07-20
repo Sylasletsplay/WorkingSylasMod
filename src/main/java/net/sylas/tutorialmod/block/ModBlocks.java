9ca6472d5d5ffa7d7b918e740a20c35d1709834f
Sylasletsplay <sylasletsplay@gmail.com>
2022-07-20 14:53:58 +0200

    A few changes but huge for me lol

---
+++ src/main/java/net/sylas/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,45 @@
+package net.sylas.tutorialmod.block;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
+import net.minecraft.client.gl.Uniform;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
+import net.minecraft.util.registry.Registry;
+import net.sylas.tutorialmod.Sylas;
+import net.sylas.tutorialmod.item.ModItemGroup;
+import org.apache.commons.compress.compressors.lz77support.LZ77Compressor;
+
+public class ModBlocks {
+    public static final Block CHADONITE_BLOCK = registerBlock("chadonite_block",
+            new Block(FabricBlockSettings.of(Material.METAL).luminance((4)).strength(5f)),ModItemGroup.MODITEMS);
+
+    public static final Block CHADONITE_ORE = registerBlock("chadonite_ore",
+            new OreBlock(FabricBlockSettings.of(Material.STONE).luminance((4)).strength(5f).requiresTool(),
+                    UniformIntProvider.create(10, 30)),ModItemGroup.MODITEMS);
+
+    public static final Block DEEPSLATE_CHADONITE_ORE = registerBlock("deepslate_chadonite_ore",
+            new OreBlock(FabricBlockSettings.of(Material.STONE).luminance((4)).strength(7f).requiresTool(),
+                    UniformIntProvider.create(10, 30)),ModItemGroup.MODITEMS);
+
+    private static Block registerBlock(String name, Block block, ItemGroup tab) {
+        registerBlockItem(name, block, tab);
+        return Registry.register(Registry.BLOCK, new Identifier(Sylas.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block, ItemGroup tab) {
+        return Registry.register(Registry.ITEM, new Identifier(Sylas.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings().group(tab)));
+    }
+
+
+    public static void registerModBlocks() {
+        Sylas.LOGGER.debug("Registering ModBlocks for " + Sylas.MOD_ID);
+    }
+}
---
+++ src/main/java/net/sylas/tutorialmod/item/ModItemGroup.java
@@ -0,0 +1,13 @@
+package net.sylas.tutorialmod.item;
+
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.sylas.tutorialmod.Sylas;
+
+public class ModItemGroup {
+
+    public static final ItemGroup MODITEMS = FabricItemGroupBuilder.build(
+            new Identifier(Sylas.MOD_ID, "moditems"), () ->new ItemStack(ModItems.CHADONITE));
+}
