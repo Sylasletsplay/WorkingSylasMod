131794df3d9ef65c703ee0a068c6b0a58f9906af
Sylasletsplay <sylasletsplay@gmail.com>
2022-07-20 14:53:58 +0200

    Initial commit

---
+++ src/main/java/net/sylas/tutorialmod/SylasClient.java
@@ -0,0 +1,10 @@
+package net.sylas.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SylasClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
